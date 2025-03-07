f345a992d82200fe395d8ddf79c1f3fff131f545
Diego Efren Elizalde Barboza <potenciapanadera@gmail.com>
2025-03-06 18:25:18 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+      <div className='text-lg text-gray-700'>
+        Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
