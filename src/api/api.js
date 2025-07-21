ccd6b5ff9e79731e5c4ee175e0dda9063dd65691
DiegoB <potenciapanadera@gmail.com>
2025-07-21 11:14:17 -0600

    Rewrite auth

--- src/api/api.js
+++ src/api/api.js
@@ -1,10 +1,27 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: 'http://localhost:8000',
+const API_KEY = import.meta.env.VITE_API_KEY;
+
+const BASE_URL = import.meta.env.VITE_BACKEND_URL
+
+export const api = axios.create({
+  // baseURL: 'http://localhost:5000/api',
+  baseURL: BASE_URL,
 });
 
-api.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('auth_token')}`;
+api.interceptors.request.use((config) => {
+  const jwt = localStorage.getItem('auth_token')
+  if(jwt) {
+    config.headers.Authorization = `Bearer ${jwt}`
+  }
 
-export default api
+  config.headers['x-api-key'] = API_KEY;
+
+  return config;
+})
+
+// api.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('auth_token')}`;
+
+// export default api;
+
 
--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import api from '../api/api';
+import { api } from '../api/api';
 
 const authSlice = createSlice({
   name: 'auth',
