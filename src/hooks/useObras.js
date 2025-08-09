f243ea4893b4f71b04bc66f2816304e9c3669bd2
AloMtz <118926524+AloMtz@users.noreply.github.com>
2025-08-09 07:00:11 -0600

    modulo de obras

--- src/hooks/useObras.js
+++ src/hooks/useObras.js
@@ -1,19 +1,24 @@
 import { api } from "../api/api"
 
 export function useObras() {
+  // Crear nueva obra
   const createObra = (data) => api.post("obras/add", data)
 
+  // Listar obras
   const listObras = ({ page = 1, limit = 10, search = "", order = "ASC" } = {}) => {
     const safeLimit = limit === 0 ? 100 : limit
     const params = { page, limit: safeLimit, search, order }
     return api.get("obras/all_obras", { params })
   }
 
-  const getOneObra = (id) => api.get(`obras/find_obras/${id}`)
+  // Obtener una obra por ID
+  const getOneObra = (id) => api.get(`obras/find_obra/${id}`)
 
-  const deleteObra = (id) => api.delete(`obras/delete_obras/${id}`)
+  // Eliminar una obra
+  const deleteObra = (id) => api.delete(`obras/delete_obra/${id}`)
 
-  const updateObra = (id, data) => api.patch(`obras/update_obras/${id}`, data)
+  // Actualizar una obra
+  const updateObra = (id, data) => api.patch(`obras/update_obra/${id}`, data)
 
   return {
     createObra,
@@ -23,4 +28,5 @@ export function useObras() {
     updateObra
   }
 }
+
 
