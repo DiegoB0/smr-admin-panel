1d14f8d8170938930c657ed28accc0e96911fb5f
AloMtz <118926524+AloMtz@users.noreply.github.com>
2025-08-09 07:28:56 -0600

    modulo de obras

--- src/hooks/useObras.js
+++ src/hooks/useObras.js
@@ -1,42 +1,28 @@
 import { api } from "../api/api"
 
 export function useObras() {
+  // Crear nueva obra
   const createObra = (data) => api.post("obras/add", data)
 
-
+  // Listar obras
   const listObras = ({ page = 1, limit = 10, search = "", order = "ASC" } = {}) => {
     const safeLimit = limit === 0 ? 100 : limit
-    const params = {
-      page,
-      limit: safeLimit,
-      search,
-      order,
-    };
+    const params = { page, limit: safeLimit, search, order }
     return api.get("obras/all_obras", { params })
   }
 
-  const listAllowedObras = ({ page = 1, limit = 10, search = "", order = "ASC", almacenId } = {}) => {
-    const safeLimit = limit === 0 ? 100 : limit
-    const params = {
-      page,
-      limit: safeLimit,
-      search,
-      order,
-      ...(almacenId ? { almacenId } : {}),
-    };
-    return api.get("obras/allowed_obras", { params })
-  }
+  // Obtener una obra por ID
+  const getOneObra = (id) => api.get(`obras/find_obra/${id}`)
 
-  const getOneObra = (id) => api.get(`obras/find_obras/${id}`)
+  // Eliminar una obra
+  const deleteObra = (id) => api.delete(`obras/delete_obra/${id}`)
 
-  const deleteObra = (id) => api.delete(`obras/delete_obras/${id}`)
-
-  const updateObra = (id, data) => api.patch(`obras/update_obras/${id}`, data)
+  // Actualizar una obra
+  const updateObra = (id, data) => api.patch(`obras/update_obra/${id}`, data)
 
   return {
     createObra,
     listObras,
-    listAllowedObras,
     getOneObra,
     deleteObra,
     updateObra
